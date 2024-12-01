f06de5ecf91d76f410387b7e10601528f5e91636
cappiwong <cappiw7@gmail.com>
2024-12-02 01:00:19 +0800

    implement double dqn

--- agent.py
+++ agent.py
@@ -13,7 +13,7 @@ LR = 0.001
 # LR = 0.001 -> n_games = 25 avg score = 0.03
 # LR = 0.01 -> n_games = 25 avg score = 0.03
 
-class Agent:
+class Agent:    
 
     def __init__(self):
         self.n_games = 0
@@ -24,51 +24,99 @@ class Agent:
         self.trainer = QTrainer(self.model, lr=LR, gamma=self.gamma)
 
 
+    # def get_state(self, game):
+    #     head = game.snake[0]
+    #     point_l = Point(head.x - 20, head.y)
+    #     point_r = Point(head.x + 20, head.y)
+    #     point_u = Point(head.x, head.y - 20)
+    #     point_d = Point(head.x, head.y + 20)
+        
+    #     dir_l = game.direction == Direction.LEFT
+    #     dir_r = game.direction == Direction.RIGHT
+    #     dir_u = game.direction == Direction.UP
+    #     dir_d = game.direction == Direction.DOWN
+
+    #     state = [
+    #         # Danger straight
+    #         (dir_r and game.is_collision(point_r)) or 
+    #         (dir_l and game.is_collision(point_l)) or 
+    #         (dir_u and game.is_collision(point_u)) or 
+    #         (dir_d and game.is_collision(point_d)),
+
+    #         # Danger right
+    #         (dir_u and game.is_collision(point_r)) or 
+    #         (dir_d and game.is_collision(point_l)) or 
+    #         (dir_l and game.is_collision(point_u)) or 
+    #         (dir_r and game.is_collision(point_d)),
+
+    #         # Danger left
+    #         (dir_d and game.is_collision(point_r)) or 
+    #         (dir_u and game.is_collision(point_l)) or 
+    #         (dir_r and game.is_collision(point_u)) or 
+    #         (dir_l and game.is_collision(point_d)),
+            
+    #         # Move direction
+    #         dir_l,
+    #         dir_r,
+    #         dir_u,
+    #         dir_d,
+            
+    #         # Food location 
+    #         game.food.x < game.head.x,  # food left
+    #         game.food.x > game.head.x,  # food right
+    #         game.food.y < game.head.y,  # food up
+    #         game.food.y > game.head.y  # food down
+    #         ]
+
+    #     return np.array(state, dtype=int)
     def get_state(self, game):
         head = game.snake[0]
         point_l = Point(head.x - 20, head.y)
         point_r = Point(head.x + 20, head.y)
         point_u = Point(head.x, head.y - 20)
         point_d = Point(head.x, head.y + 20)
-        
+
         dir_l = game.direction == Direction.LEFT
         dir_r = game.direction == Direction.RIGHT
         dir_u = game.direction == Direction.UP
         dir_d = game.direction == Direction.DOWN
 
+        # Add distances to food
+        food_dx = (game.food.x - head.x) / game.w
+        food_dy = (game.food.y - head.y) / game.h
+
         state = [
             # Danger straight
-            (dir_r and game.is_collision(point_r)) or 
-            (dir_l and game.is_collision(point_l)) or 
-            (dir_u and game.is_collision(point_u)) or 
+            (dir_r and game.is_collision(point_r)) or
+            (dir_l and game.is_collision(point_l)) or
+            (dir_u and game.is_collision(point_u)) or
             (dir_d and game.is_collision(point_d)),
 
             # Danger right
-            (dir_u and game.is_collision(point_r)) or 
-            (dir_d and game.is_collision(point_l)) or 
-            (dir_l and game.is_collision(point_u)) or 
+            (dir_u and game.is_collision(point_r)) or
+            (dir_d and game.is_collision(point_l)) or
+            (dir_l and game.is_collision(point_u)) or
             (dir_r and game.is_collision(point_d)),
 
             # Danger left
-            (dir_d and game.is_collision(point_r)) or 
-            (dir_u and game.is_collision(point_l)) or 
-            (dir_r and game.is_collision(point_u)) or 
+            (dir_d and game.is_collision(point_r)) or
+            (dir_u and game.is_collision(point_l)) or
+            (dir_r and game.is_collision(point_u)) or
             (dir_l and game.is_collision(point_d)),
-            
+
             # Move direction
             dir_l,
             dir_r,
             dir_u,
             dir_d,
-            
-            # Food location 
-            game.food.x < game.head.x,  # food left
-            game.food.x > game.head.x,  # food right
-            game.food.y < game.head.y,  # food up
-            game.food.y > game.head.y  # food down
-            ]
 
-        return np.array(state, dtype=int)
+            # Normalized distances to food
+            food_dx,
+            food_dy,
+        ]
+
+        return np.array(state, dtype=float)
+
 
     def remember(self, state, action, reward, next_state, done):
         self.memory.append((state, action, reward, next_state, done)) # popleft if MAX_MEMORY is reached
@@ -136,6 +184,10 @@ def train():
             if score > record:
                 record = score
                 agent.model.save()
+            
+            # Update target network every 10 games
+            if agent.n_games % 10 == 0:
+                agent.trainer.update_target_network()
 
             print('Game', agent.n_games, 'Score', score, 'Record:', record)
 
