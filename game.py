8ea854d6983183d058ada663fa363b7f6e73ecfd
cappiwong <cappiw7@gmail.com>
2024-12-01 21:54:34 +0800

    add portal features

--- game.py
+++ game.py
@@ -28,7 +28,6 @@ BLOCK_SIZE = 20
 SPEED = 40
 
 class SnakeGameAI:
-
     def __init__(self, w=640, h=480):
         self.w = w
         self.h = h
@@ -38,29 +37,40 @@ class SnakeGameAI:
         self.clock = pygame.time.Clock()
         self.reset()
 
-
     def reset(self):
         # init game state
         self.direction = Direction.RIGHT
-
-        self.head = Point(self.w/2, self.h/2)
-        self.snake = [self.head,
-                      Point(self.head.x-BLOCK_SIZE, self.head.y),
-                      Point(self.head.x-(2*BLOCK_SIZE), self.head.y)]
-
+        self.head = Point(self.w / 2, self.h / 2)
+        self.snake = [
+            self.head,
+            Point(self.head.x - BLOCK_SIZE, self.head.y),
+            Point(self.head.x - (2 * BLOCK_SIZE), self.head.y),
+        ]
         self.score = 0
         self.food = None
+        self.portals = []
         self._place_food()
+        self._place_portals()
         self.frame_iteration = 0
 
-
     def _place_food(self):
-        x = random.randint(0, (self.w-BLOCK_SIZE )//BLOCK_SIZE )*BLOCK_SIZE
-        y = random.randint(0, (self.h-BLOCK_SIZE )//BLOCK_SIZE )*BLOCK_SIZE
+        x = random.randint(0, (self.w - BLOCK_SIZE) // BLOCK_SIZE) * BLOCK_SIZE
+        y = random.randint(0, (self.h - BLOCK_SIZE) // BLOCK_SIZE) * BLOCK_SIZE
         self.food = Point(x, y)
-        if self.food in self.snake:
+        if self.food in self.snake or any(x == self.food for x in self.portals):
             self._place_food()
 
+    def _place_portals(self):
+        self.portals = []
+        for _ in range(2):  # Place 2 portals
+            x = random.randint(0, (self.w - BLOCK_SIZE) // BLOCK_SIZE) * BLOCK_SIZE
+            y = random.randint(0, (self.h - BLOCK_SIZE) // BLOCK_SIZE) * BLOCK_SIZE
+            portal = Point(x, y)
+            while portal in self.snake or portal == self.food or portal in self.portals:
+                x = random.randint(0, (self.w - BLOCK_SIZE) // BLOCK_SIZE) * BLOCK_SIZE
+                y = random.randint(0, (self.h - BLOCK_SIZE) // BLOCK_SIZE) * BLOCK_SIZE
+                portal = Point(x, y)
+            self.portals.append(portal)
 
     def play_step(self, action):
         self.frame_iteration += 1
@@ -69,33 +79,45 @@ class SnakeGameAI:
             if event.type == pygame.QUIT:
                 pygame.quit()
                 quit()
-        
+
         # 2. move
-        self._move(action) # update the head
+        self._move(action)  # update the head
         self.snake.insert(0, self.head)
-        
-        # 3. check if game over
+
+        # 3. check for portals
+        self._check_portals()
+
+        # 4. check if game over
         reward = 0
         game_over = False
-        if self.is_collision() or self.frame_iteration > 100*len(self.snake):
+        if self.is_collision() or self.frame_iteration > 100 * len(self.snake):
             game_over = True
             reward = -10
             return reward, game_over, self.score
 
-        # 4. place new food or just move
+        # 5. place new food or just move
         if self.head == self.food:
             self.score += 1
             reward = 10
             self._place_food()
         else:
             self.snake.pop()
-        
-        # 5. update ui and clock
+
+        # 6. update ui and clock
         self._update_ui()
         self.clock.tick(SPEED)
-        # 6. return game over and score
+        # 7. return game over and score
         return reward, game_over, self.score
 
+    def _check_portals(self):
+        for i in range(0, len(self.portals), 2):
+            if self.head == self.portals[i]:
+                # Teleport to the paired portal
+                self.head = Point(self.portals[i + 1].x, self.portals[i + 1].y)
+                break
+            elif self.head == self.portals[i + 1]:
+                self.head = Point(self.portals[i].x, self.portals[i].y)
+                break
 
     def is_collision(self, pt=None):
         if pt is None:
@@ -109,35 +131,40 @@ class SnakeGameAI:
 
         return False
 
-
     def _update_ui(self):
         self.display.fill(BLACK)
 
+        # Draw the snake
         for pt in self.snake:
             pygame.draw.rect(self.display, BLUE1, pygame.Rect(pt.x, pt.y, BLOCK_SIZE, BLOCK_SIZE))
-            pygame.draw.rect(self.display, BLUE2, pygame.Rect(pt.x+4, pt.y+4, 12, 12))
+            pygame.draw.rect(self.display, BLUE2, pygame.Rect(pt.x + 4, pt.y + 4, 12, 12))
 
+        # Draw the food
         pygame.draw.rect(self.display, RED, pygame.Rect(self.food.x, self.food.y, BLOCK_SIZE, BLOCK_SIZE))
 
+        # Draw portals
+        for i, portal in enumerate(self.portals):
+            color = (0, 255, 0) if i % 2 == 0 else (255, 0, 255)
+            pygame.draw.rect(self.display, color, pygame.Rect(portal.x, portal.y, BLOCK_SIZE, BLOCK_SIZE))
+
+        # Display score
         text = font.render("Score: " + str(self.score), True, WHITE)
         self.display.blit(text, [0, 0])
         pygame.display.flip()
 
-
     def _move(self, action):
         # [straight, right, left]
-
         clock_wise = [Direction.RIGHT, Direction.DOWN, Direction.LEFT, Direction.UP]
         idx = clock_wise.index(self.direction)
 
         if np.array_equal(action, [1, 0, 0]):
-            new_dir = clock_wise[idx] # no change
+            new_dir = clock_wise[idx]  # no change
         elif np.array_equal(action, [0, 1, 0]):
             next_idx = (idx + 1) % 4
-            new_dir = clock_wise[next_idx] # right turn r -> d -> l -> u
-        else: # [0, 0, 1]
+            new_dir = clock_wise[next_idx]  # right turn
+        else:  # [0, 0, 1]
             next_idx = (idx - 1) % 4
-            new_dir = clock_wise[next_idx] # left turn r -> u -> l -> d
+            new_dir = clock_wise[next_idx]  # left turn
 
         self.direction = new_dir
 
